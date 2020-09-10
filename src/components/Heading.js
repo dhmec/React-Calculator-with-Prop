44e714c27eb93b0a7d31bfcf4b8ebc83bee6b201
dhmec <dharmoradiya@gmail.com>
2020-09-10 14:49:21 -0600

    Created Heading component for the main heading;

---
+++ src/components/Heading.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Heading () 
+{
+    return (
+        <h1>
+            Welcome to my Calculator!
+        </h1>
+    );
+}
+
+export default Heading;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Heading from './components/Heading';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <></>
+    <Heading />
   </React.StrictMode>,
   document.getElementById('root')
 );
