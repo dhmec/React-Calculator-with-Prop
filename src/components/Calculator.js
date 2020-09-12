3281dd1413b6640fc3dedb9985676ce99b143f4f
dhmec <dharmoradiya@gmail.com>
2020-09-12 14:08:14 -0600

    add submit event

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,27 +3,46 @@ import React, { useState } from 'react';
 
 const Calculator = props => {
     
-    
+    let result;
     const [newFirstInput, setNewFirstInput] = useState( '' );
     const [newSecondInput, setNewSecondInput] = useState( '' );
-
-    const result = parseFloat(newFirstInput) + parseFloat(newSecondInput);
+    const [newOption, setNewOption] = useState( 'addition' );
+    const firstInputNumber = parseFloat(newFirstInput);
+    const secondInputNumber = parseFloat(newSecondInput);
+   
+    const displayCalculate  = event => {
+        event.preventDefault();
+        console.log('clicked');
+        
+        if (newOption ==='addition')
+        {
+            console.log('add selected');
+            result =  firstInputNumber + secondInputNumber;
+            console.log(result);
+        }
+        else if (newOption === 'substraction')
+        {
+            console.log('substraction selected');
+            result =  firstInputNumber - secondInputNumber;
+            console.log(result);
+        }
+        else if (newOption === 'multiplication')
+        {
+            console.log('multiplication selected');
+            result =  firstInputNumber * secondInputNumber;
+            console.log(result);
+        }
+        else 
+        {
+            console.log('division selected');
+            result =  firstInputNumber / secondInputNumber;
+            console.log(result);
+        }
+                
+    }  
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+       
     
     return (
       
@@ -31,7 +50,7 @@ const Calculator = props => {
             <h1>
                 Welcome to my Calculator!
             </h1> 
-            <form>
+            <form onSubmit={displayCalculate}>
                 <label htmlFor="firstInput">Input 1:</label>
                 <input type="text" 
                        id="firstInput"
@@ -39,7 +58,7 @@ const Calculator = props => {
                        value={newFirstInput} />
                 
                 <label htmlFor="selectBox"> Operation:</label>
-                <select id="selectBox" name="selectBox" htmlFor="selectBox">
+                <select id="selectBox" onChange={e => {setNewOption (e.target.value)}} value={newOption} htmlFor="selectBox">
                         <option value="addition">+ (Add)</option>
                         <option value="substraction">- (Sub)</option>
                         <option value="multiplication">* (Mul)</option>
@@ -54,7 +73,7 @@ const Calculator = props => {
 
                 <input type='submit' value="Calculate!" />
             </form>
-            <p>Result : {result}</p>
+            <p>Result : {result} </p>
         </div>
       
     );
